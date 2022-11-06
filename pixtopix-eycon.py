aa846ef45d729d82b9ebdc5b80556de6f7f07d49
aissi salim <saissi1701@gmail.com>
2022-11-06 10:03:35 +0100

    Add files via upload

--- pixtopix-eycon.py
+++ pixtopix-eycon.py
@@ -14,6 +14,7 @@ import random
 from torch.utils.data import Dataset, DataLoader
 from torchvision.utils import save_image
 import os
+import subprocess
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 print(DEVICE)
 
