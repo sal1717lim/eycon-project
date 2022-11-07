39f807cb8135e1b08245b1b8fe96e8e31749294a
aissi salim <saissi1701@gmail.com>
2022-11-07 20:20:54 +0100

    Add files via upload

---
+++ run.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int main() {
+	pid_t pid = fork();
+	if (pid == -1) {
+		// Il y a une erreur
+		perror("fork");
+		return EXIT_FAILURE;
+	} else if (pid == 0) {
+		// On est dans le fils
+		printf("Mon PID est %i et celui de mon père est %i\n", getpid(),	getppid());
+		system( "python3 script2.py 100 20" );
+	} else {
+		// On est dans le père
+		printf("Mon PID est %i et celui de mon fils est %i\n", getpid(), pid);
+	}
+	return EXIT_SUCCESS;
+}
