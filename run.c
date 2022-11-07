f23cd191aa5f7afb40b3b82af6faaff1a22162d9
aissi salim <saissi1701@gmail.com>
2022-11-07 20:30:37 +0100

    Add files via upload

--- run.c
+++ run.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
 
 int main() {
-	pid_t pid = fork();
+	int pid = fork();
 	if (pid == -1) {
 		// Il y a une erreur
 		perror("fork");
